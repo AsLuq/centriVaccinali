2119361f97abc15ef741eadede548219fe5f952c
Cri Zuffo <69976118+Crizuffo@users.noreply.github.com>
2021-11-15 16:05:59 +0100

    Implementato la verifica del numero civico, della provincia e del cap

--- src/main/java/com/studenti/uninsubria/clientCV/utility/Utility.java
+++ src/main/java/com/studenti/uninsubria/clientCV/utility/Utility.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 /**
  * @author luqmanasghar
+ * @author Cristian Zuffellato
  */
 public class Utility {
 
@@ -24,4 +25,47 @@ public class Utility {
         Matcher matcher = pattern.matcher(cf);
         return matcher.matches();
     }
+
+    /**
+     * Controlla che la lunghezza del civico non sia superiore a 5 caratteri.
+     * Controlla che il civico non superi il 14159, che è il numero civico max italiano.
+     * @param civico
+     * @return boolean
+     */
+
+    public boolean isCivicoValid(String civico){
+        if(civico.length() <= 5 && Integer.parseInt(civico) <= 14159){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * controlla che la lunghezza della provincia sia di due caratteri ed inoltre che non vengano inseriti caratteri al di fuori di lettere
+     * @return boolean
+     */
+
+    public boolean isProvinciaValid(String provincia){
+
+        if(provincia.length() < 2 && provincia.matches("^[a-zA-Z]+$")){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Controlla se il cap è minore di 98168, in quanto numero di cap maggiore in Italia;
+     * controlla se il cap è maggiore di 9, in quanto è il numero minore di cap presente in Italia;
+     * Controlla se la sua lunghezza è uguale 5.
+     *
+     * @return boolean
+     */
+
+    public boolean isCapValid(String cap){
+        if(Integer.parseInt(cap) < 98168 && Integer.parseInt(cap) > 9 && cap.length() == 5){
+            return true;
+        }
+        return false;
+    }
+
 }
